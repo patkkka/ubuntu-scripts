2b793a7a3f143ffc3af1f6c58a9a30b4e55a9035
pat <klimczak.patrycja@gmail.com>
2017-11-10 19:40:26 +0100

    Maven, Gradle and MongoDB installation added

--- SonarInstallation.sh
+++ SonarInstallation.sh
@@ -2,12 +2,11 @@
 
 set -e 
 
-wget --output-document=sonar.zip -nc 'https://akamai.bintray.com/90/901df25f99cc28b8dd78325153f4d3ba234417d3d9d42df91d811a26e554f2e4?__gda__=exp=1509358349~hmac=04d7ff777131e3b81dcd72ea121a6e3d46698f21b1e8b0438a5760fca8862485&response-content-disposition=attachment%3Bfilename%3D%22sonarqube-6.6.zip%22&response-content-type=application%2Fzip&requestInfo=U2FsdGVkX19Bxo2a3f8ZU25rMAr8VG76sTDaCznbtUCO10xSJ0bN6MXGHIkkGL20eUjTPV0-bRjcEnybEM_Ai4vAoW1PChShD8z6KNLKvGleczjNL3i6TlTxJoCvA0NVXugusG3PsOXl3FslRUwPFwkGftevu_IDXeE2srXs2ofjHVfnWxZ8R9i01m_yoFJt&response-X-Checksum-Sha1=79ccba38c2228c218bca59f9c5cd39b6bee48dc8&response-X-Checksum-Sha2=901df25f99cc28b8dd78325153f4d3ba234417d3d9d42df91d811a26e554f2e4&response-X-Checksum-Sha256=901df25f99cc28b8dd78325153f4d3ba234417d3d9d42df91d811a26e554f2e4'
+wget --output-document=sonar.zip -nc 'https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-6.6.zip'
 
-unzip sonar.zip
+#due to permissions problem...
+mkdir ~/sonar/
 
-mv sonarqube-6.6 /opt/sonar
-
-mv /opt/sonarqube-6.6 /opt/sonar
+unzip sonar.zip -d ~/sonar/
 
 rm sonar.zip
--- importAliases.sh
+++ importAliases.sh
@@ -6,5 +6,8 @@ echo "alias sonarStart='cd /opt/sonar/bin/linux-x86-64 && ./sonar.sh start'" >> 
 echo "alias sonarStop='cd /opt/sonar/bin/linux-x86-64 && ./sonar.sh stop'" >> ~/.zshrc
 echo "alias mci='mvn clean install'" >> ~/.zshrc
 echo "alias mciss='mvn clean install sonar:sonar'" >> ~/.zshrc
-echo "alias mvnAll='mvn clean install jacoco:report site:site sonar:sonar" >> ~/.zshrc
+echo "alias mvnall='mvn clean install jacoco:report site:site sonar:sonar'" >> ~/.zshrc
+echo "alias gac='alias gac='git add . && git commit -m'" >> ~/.zshrc
+echo "alias mongoStart='sudo service mongod start'" >> ~/.zshr
+
 
